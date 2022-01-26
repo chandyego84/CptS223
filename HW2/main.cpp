71a3bf32a23c8fc34d3254eb6415cadab0ec2bb5
chandyego <chandlerjuego101@gmail.com>
2022-01-25 21:40:28 -0800

    init of 100 vectors of size 100

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,11 +1,18 @@
 #include "indexEqual.h"
 
 int main() {
-    vector<int> A;
-    
+    // 100 vectors of size 100
+    vector<vector<int>> A(100);
+    // initialize each vector
+    for (int i=0; i<100; i++) {
+        for (int j=-1; j<99; j++) {
+            if (j == 98) A[i].push_back(j+1); // kth element of kth array equals index
+            else A[i].push_back(j);
+        }
+    }
 
-    cout << "Recursive Index Equal: " << IndexEqualRecurs(A, 0, 99) << endl;
-    cout << "Iterative Index Equal: " << IndexEqualIter(A, 0, 99) << endl;
+    //cout << "Recursive Index Equal: " << IndexEqualRecurs(A, 0, 99) << endl;
+    //cout << "Iterative Index Equal: " << IndexEqualIter(A, 0, 99) << endl;
 
     return 0;
 }
